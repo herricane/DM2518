45c638e33e1d0e703137318eb0f412074d7da2b7
herricane <xu6648@gmail.com>
2020-04-29 12:14:38 +0200

    chat update

--- lab4/index.js
+++ lab4/index.js
@@ -1,28 +1,29 @@
-var pubnubDemo = new PubNub({
+const Words = document.getElementById("words");
+const TalkWords = document.getElementById("talkwords");
+const TalkSub = document.getElementById("talksub");
+TalkSub.addEventListener('click', () => {submitUpdate(TalkWords.value)})
+
+const pubnubDemo = new PubNub({
   publishKey: 'pub-c-b931c396-73b9-4718-8c6b-4571e7959567',
   subscribeKey: 'sub-c-cc3aa0a0-846f-11ea-885f-2621b2dc68c7'
 });
-pubnubDemo.publish({ message: { "color": "blue" }, channel: 'demo_tutorial' });
+
 pubnubDemo.addListener({
-  message: function (message) {
-    console.log(message)
+  message: function (event) {
+    let str = '<div class="atalk"><span>' + event.message.update + '</span></div>';
+    Words.innerHTML = Words.innerHTML + str;
   }
 })
+
 pubnubDemo.subscribe({ channels: ['demo_tutorial'] });
 
-window.onload = function () {
-  var Words = document.getElementById("words");
-  var TalkWords = document.getElementById("talkwords");
-  var TalkSub = document.getElementById("talksub");
-
-  TalkSub.onclick = function () {
-    var str = "";
-    if (TalkWords.value == "") {
-      alert("Can't send empty message.");
-      return;
-    }
-    str = '<div class="btalk"><span>' + TalkWords.value + '</span></div>';
+submitUpdate = function (update) {
+  pubnubDemo.publish({
+    channel: 'demo_tutorial',
+    message: { 'update': update }
+  }, function () {
+    let str = '<div class="btalk"><span>' + update + '</span></div>';
     Words.innerHTML = Words.innerHTML + str;
     TalkWords.value = ""
-  }
+  })
 }
